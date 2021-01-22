9fa0c99f5c94fb427b0c7a3a62c51beb5722d7e2
Alistair Spragg <thespragg@pop-os.localdomain>
2021-01-22 20:12:22 +0000

    Imported font awesome and required icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueMq from "vue3-mq"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLightbulb, faChartBar} from '@fortawesome/free-regular-svg-icons'
+import { faUserTie} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faLightbulb, faUserTie, faChartBar)
 
 const app = createApp(App)
   .use(store)
@@ -15,4 +21,6 @@ const app = createApp(App)
       xl: Infinity,
     }
   })
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
